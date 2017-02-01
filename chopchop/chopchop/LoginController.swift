7cd495252291053241e6494f4f82e74c8d81beaf
Maor Eini <maor.eini@gmail.com>
2017-02-01 05:35:01 +0200

    ME: Add initial workspace with firebase

---
+++ chopchop/chopchop/LoginController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chopchop
+//
+//  Created by Maor Eini on 01/02/2017.
+//  Copyright © 2017 Maor Eini. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
