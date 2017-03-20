206fca283da4d89abc1a653602076b3d2012ed89
Maor Eini <maor.eini@gmail.com>
2017-03-20 20:35:18 +0200

    ME: Order by desc for feed items

--- chopchop/chopchop/Feed+sql.swift
+++ chopchop/chopchop/Feed+sql.swift
@@ -109,7 +109,7 @@ extension FeedItem{
         var FeedItems = [FeedItem]()
         var sqlite3_stmt: OpaquePointer? = nil
         
-        if (sqlite3_prepare_v2(database,"SELECT * from FEED;",-1,&sqlite3_stmt,nil) == SQLITE_OK){
+        if (sqlite3_prepare_v2(database,"SELECT * from FEED ORDER BY date DESC;",-1,&sqlite3_stmt,nil) == SQLITE_OK){
             while(sqlite3_step(sqlite3_stmt) == SQLITE_ROW) {
                 
                 let id =  String(validatingUTF8:sqlite3_column_text(sqlite3_stmt,0))
